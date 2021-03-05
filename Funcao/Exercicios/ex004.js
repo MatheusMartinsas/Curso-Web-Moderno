a61bcebbe1e87ac01ee27115f87390bbe9c2bb95
Matheus Martins Barbosa <matheusmartinsas@gmail.com>
2021-03-05 14:20:26 -0300

    Create ex004.js

---
+++ Funcao/Exercicios/ex004.js
@@ -0,0 +1,7 @@
+function divisão(dividendo, divisor) {
+    let resultado = dividendo / divisor
+    let resto = dividendo % divisor
+    return console.log(`O resultado da divisão é igaul a ${resultado}, sobrando ${resto}.`)
+}
+
+divisão(10, 3)
