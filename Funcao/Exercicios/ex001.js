7502a6ee46189f8f78581614732dc428f9f2b091
Matheus Martins Barbosa <matheusmartinsas@gmail.com>
2021-03-05 14:44:49 -0300

    Exercícios Resolvidos

--- Funcao/Exercicios/ex001.js
+++ Funcao/Exercicios/ex001.js
@@ -1,8 +1,5 @@
 function calcular(v1, v2) {
-    console.log(v1 + v2)
-    console.log(v1 - v2)
-    console.log(v1 * v2)
-    console.log(v1 / v2)
+    console.log(v1 + v2, v1 - v2, v1 * v2, v1 / v2)
 }
 
 calcular(2, 3)
---
+++ Funcao/Exercicios/ex005.js
@@ -0,0 +1,5 @@
+function conversor(n) {
+    return console.log(`R$ ${n.toFixed(2).replace(".", ",")}`)
+}
+
+conversor(50.78954516)
